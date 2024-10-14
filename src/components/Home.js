c9c316194c8577d0165f93d52672f60a40f9f4e1
Huong Nguyen <hnguyen@huongs-air-2.mynetworksettings.com>
2024-10-13 23:51:09 -0700

    First iteration of home page content section

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,11 @@ import Card from '@mui/material/Card';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 
 // from MUI Grid component documentation
 const Item = styled(Card)(({ theme }) => ({
-    backgroundColor: 'blue',
+    backgroundColor: '#1d1b1d',
     //'#1d1b1d'''
     borderRadius: 20, // makes cards round
 
@@ -28,20 +29,24 @@ function Home(){
         <Box className="home-container" backgroundColor="#141314">
         {/* sx={{ flexGrow: 1 }} */}
         {/* flexGrow: 1 is a CSS Flexbox property. It tells the flex item (the Box component) to take up any remaining space in the flex container.  */}
-          <Box className="home-main">
+          <Box className="home-main" >
 
 
            {/* 
            Header  
            */}
-          <Grid container spacing={1} className="header-container"> 
+          <Grid container spacing={1} className="header-container" margin="10px" paddingTop="10px"> 
           {/* alignItems="stretch" */}
             <Grid size={{sm: 12, md: 6 }} display="flex" className="title-container"> 
             {/* display="flex" */}
             {/* the view is split into 12, so 6 means half the view */}
               <Item height="100%" className="title-wrapper">
-              <Typography variant="h1" sx={{ fontFamily: 'Roboto, Arial, sans-serif', fontSize: '7rem', fontWeight: '400', color:"#fff" }}>Huong Nguyen</Typography>                
-              <Typography variant="h6" sx={{ fontFamily: 'Roboto, Arial, sans-serif', fontWeight: '200', color:"#fff" }} className="description">Huong N. is a full stack developer who enjoys making kick ass apps. Develop and enjoy beautiful, usable products with Huong's expertise.</Typography>
+              <Typography variant="h1" sx={{ fontFamily: 'Roboto, Arial, sans-serif', fontSize: '7rem', fontWeight: '400', color:"#fff" }}>
+                Huong Nguyen
+              </Typography>                
+              <Typography variant="h6" sx={{ fontFamily: 'Roboto, Arial, sans-serif', fontWeight: '200', color:"#fff" }} className="description">
+                Huong N. is a full stack developer who enjoys making kick ass apps. Develop and enjoy beautiful, usable products with Huong's expertise.
+              </Typography>
               <Button href="https://www.linkedin.com/in/htn5400" target="_blank" rel="noopener noreferrer" variant="contained" sx={ { borderRadius: 28, color:'black', backgroundColor: '#A67AFE', fontFamily: 'Roboto, Arial, sans-serif' }}>Get started</Button>
 
               </Item>
@@ -51,7 +56,7 @@ function Home(){
               {/* flex="1" */}
                 {/* Create-react-app uses webpack under the hood so make sure you do src correctly */}
                 {/* <img src={require('./mommy.jpg')} alt="mommy's lil monster" width="600" height="600"></img> */}
-                <video autoPlay muted loop width="100%" height="100%" objectFit="contain" >
+                <video autoPlay muted loop width="100%" height="100%" objectfit="contain" >
                     <source src={require("./materialdesign.mp4")} type="video/mp4"></source>
                     Your browser does not support the video tag.
                 </video>
@@ -63,23 +68,63 @@ function Home(){
           {/* 
           Content 
           */}
-          {/* <Box className="home-content-container" display="flex" width="90%" >
-            <Box className="home-content-header"  >
-              <Typography variant="h2" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '380', color:"#fff"}}>News & launches </Typography>
+          <Box className="home-content-container" width="90%" margin="auto" padding="20px">
+            <Box className="content-section">
+            <Box className="section-header" paddingTop="50px" paddingBottom="25px">
+              <Typography variant="h2" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '400', color:"#fff"}}>
+                News & launches 
+              </Typography>
             </Box>
-            <Grid container spacing={1} className="content-cardset-container">
-              <Grid size={{sm: 12, md: 6 }} display="flex"> 
-                <Item height="100%">
-                  <Typography>HI</Typography>
+            <Grid container spacing={1} className="content-cardset-container" >
+              <Grid size={{xs: 12, sm: 6, md: 4 }} borderRadius="20px" display="flex" flexDirection="column" backgroundColor='#1d1b1d' > 
+              <Link href="https://linkedin.com/in/htn5400" target="_blank" rel="noopener noreferrer" underline="none" display="contents"> 
+                <Item height="100%" width="100%">
+                  <img src={require('./contentimg1.png')} alt="green pattern 1" width="100%" height="100%"></img>
                 </Item>
+                <Box padding="10px" margin="5%" >
+                  <Typography paddingBottom="5px" variant="h5" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '400', color:"#fff"}}>
+                    Material Design at Google
+                  </Typography>
+                  <Typography variant="body2" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '300', color:"#fff"}}>
+                  Learn more about the latest version of Google's open-source design system
+                  </Typography>
+                </Box>
+                </Link>
+
               </Grid>
-              <Grid size={{sm: 12, md: 6 }} display="flex">
-                <Item height="100%">
-                  <Typography>THERE</Typography>
+              <Grid size={{xs: 12, sm: 6, md: 4 }} borderRadius="20px" display="flex" flexDirection="column" backgroundColor='#1d1b1d' >
+              <Link href="https://linkedin.com/in/htn5400" target="_blank" rel="noopener noreferrer" underline="none" display="contents">
+                <Item height="100%" width="100%">
+                  <img src={require('./contentimg2.png')} alt="green g pattern 1" width="100%" height="100%"></img>
                 </Item>
+                <Box padding="10px" margin="5%">
+                  <Typography paddingBottom="5px" variant="h5" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '400', color:"#fff"}}>
+                    How to get Google Fonts
+                  </Typography>
+                  <Typography variant="body2" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '300', color:"#fff"}}>
+                  New API updates and website upgrades make font integraation easier than ever
+                  </Typography>
+                </Box>
+              </Link>
+              </Grid>
+              <Grid size={{xs: 12, sm: 6, md: 4}} borderRadius="20px" display="flex" flexDirection="column" backgroundColor='#1d1b1d' >
+              <Link href="https://linkedin.com/in/htn5400" target="_blank" rel="noopener noreferrer" underline="none" display="contents">  
+                <Item height="100%" width="100%">
+                  <img src={require('./contentimg3.png')} alt="purple 3 pattern 1" width="100%" height="100%"></img>
+                </Item>
+                <Box padding="10px" margin="5%">
+                  <Typography paddingBottom="5px" variant="h5" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '400', color:"#fff"}}>
+                    Figma design kit
+                  </Typography>
+                  <Typography variant="body2" sx={{ fontFamily: 'Roboto, Arial, sans-serif',  fontWeight: '300', color:"#fff"}}>
+                  Start designing and prototyping with the complete M3 library of ready-made components 
+                  </Typography>
+                </Box>
+              </Link>
               </Grid>
             </Grid>
-          </Box> */}
+            </Box>
+          </Box>
 
 
 
